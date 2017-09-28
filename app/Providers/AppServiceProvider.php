69259f3f6255f028c3c5b382b8fb6299cfc78ff8
Ralph Lagumen <ralph.lagumen@philutech.com>
2017-09-28 15:11:22 +0800

    Updated in laravel 5.5.13 *Added the following Package (require) -doctrine/dbal -jenssegers/agent -jenssegers/date -predis/predis -silber/bouncer -torann/geoip (require-dev) -spatie/laravel-mailable-test -barryvdh/laravel-ide-helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
         //
     }
 }
